bad36539978ddcc43d615de4d1e3975d4a2a4461
Aldanis Vigo <aldanisvigo@gmail.com>
2022-07-04 03:50:00 -0500

    Initial Commit

---
+++ Shared/SwitftUI_Tutorial_Pinch_GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitftUI_Tutorial_Pinch_GestureApp.swift
+//  Shared
+//
+//  Created by Aldanis Vigo on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SwitftUI_Tutorial_Pinch_GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
